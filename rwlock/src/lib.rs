6194868348658f9c901e9a44d43368f9b25d5082
Jack Dockerty <jdockerty19@gmail.com>
2024-07-02 12:48:16 +0100

    feat: simple rwlock

--- rwlock/src/lib.rs
+++ rwlock/src/lib.rs
@@ -4,7 +4,7 @@ use std::{
     sync::atomic::{AtomicU32, Ordering},
 };
 
-use atomic_wait::wake_one;
+use atomic_wait::{wait, wake_all, wake_one};
 
 pub struct WriteGuard<'a, T> {
     inner: &'a RwLock<T>,
@@ -12,10 +12,11 @@ pub struct WriteGuard<'a, T> {
 
 impl<T> Drop for WriteGuard<'_, T> {
     fn drop(&mut self) {
-        if self.inner.state.swap(0, Ordering::Release) == 2 {
-            // Wake a single waiting thread, if any
-            wake_one(&self.inner.state);
-        }
+        // Reset counter
+        self.inner.state.store(0, Ordering::Release);
+        // We don't know whether readers/writers are waiting, so wake all threads
+        // and allow the internal logic to handle the rest.
+        wake_all(&self.inner.state);
     }
 }
 
@@ -45,6 +46,17 @@ impl<T> Deref for ReadGuard<'_, T> {
     }
 }
 
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        // fetch_sub here means that if the returned value is 1, there are now
+        // no more reader locks held. Wake up a slept thread to proceed.
+        // This is because it returns what the PRIOR value was, before subtraction
+        if self.inner.state.fetch_sub(1, Ordering::Release) == 1 {
+            wake_one(&self.inner.state);
+        }
+    }
+}
+
 pub struct RwLock<T> {
     // Numbers of readers or `u32::MAX` when there is a writer lock
     state: AtomicU32,
@@ -64,7 +76,38 @@ impl<T> RwLock<T> {
         }
     }
 
-    pub fn read(&self) -> ReadGuard<'_, T> {}
+    pub fn read(&self) -> ReadGuard<T> {
+        let mut s = self.state.load(Ordering::Relaxed);
+        loop {
+            if s < u32::MAX {
+                assert!(s != u32::MAX - 1, "too many readers");
+                match self.state.compare_exchange_weak(
+                    s,
+                    s + 1,
+                    Ordering::Acquire,
+                    Ordering::Relaxed,
+                ) {
+                    Ok(_) => return ReadGuard { inner: self },
+                    Err(e) => s = e,
+                }
+            }
 
-    pub fn write(&mut self) {}
+            // Write-locked, put the thread to sleep
+            if s == u32::MAX {
+                wait(&self.state, u32::MAX);
+                s = self.state.load(Ordering::Relaxed);
+            }
+        }
+    }
+
+    pub fn write(&mut self) -> WriteGuard<T> {
+        while let Err(s) =
+            self.state
+                .compare_exchange(0, u32::MAX, Ordering::Acquire, Ordering::Relaxed)
+        {
+            // Already write-locked, so wait
+            wait(&self.state, s);
+        }
+        WriteGuard { inner: self }
+    }
 }
